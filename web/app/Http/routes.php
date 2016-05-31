cb139d37e598eb8820d72267d843b7d5164266cd
Mario Krvaric <mario.krvaric@trikoder.net>
2016-05-31 17:11:30 +0200

    user edit

--- web/app/Http/routes.php
+++ web/app/Http/routes.php
@@ -47,6 +47,11 @@ Route::group(['middleware' => 'admin'], function() {
     Route::group(['namespace' => 'Web'], function() {
 
         Route::get('dashboard', 'DashboardController@index');
+        Route::get('user/edit/{id}', 'AdminUserController@showEdit');
+        Route::post('user/save/{id}', [
+            'as' => 'user/save',
+            'uses' => 'AdminUserController@saveUser'
+        ]);
 
         Route::group(['prefix' => 'task'], function() {
             Route::group(['prefix' => 'categories'], function() {
